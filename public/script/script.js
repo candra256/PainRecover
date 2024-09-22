c7aaf18118cce807edf0aeaf8a15061c3adbfb5b
candra256 <dalangpelo982@gmail.com>
2024-09-21 21:11:06 -1200

    Initial commit

---
+++ public/script/script.js
@@ -0,0 +1,24 @@
+//_____________________________________________________________ 🔽
+
+const toggle = document.getElementsByClassName('toggle')[0];
+const menu = document.getElementsByClassName('menu')[0];
+
+// Menambahkan event listener untuk tombol toggle
+toggle.addEventListener('click', function(event) {
+    // Toggle class hidden pada menu
+    menu.classList.toggle('hidden');
+    
+    // Mencegah event bubbling agar klik di toggle tidak dianggap sebagai klik di luar menu
+    event.stopPropagation();
+});
+
+// Event listener untuk klik di luar elemen menu
+document.addEventListener('click', function(event) {
+    // Jika menu terbuka (tidak ada class hidden) dan pengguna mengklik di luar menu
+    if (!menu.classList.contains('hidden') && !menu.contains(event.target)) {
+        // Tambahkan class hidden untuk menyembunyikan menu
+        menu.classList.add('hidden');
+    }
+});
+
+//_____________________________________________________________ 🔼
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing : {
+        '29' : '6.1rem',
+      }
+    },
+  },
+  plugins: [
+  ],
+}
+
