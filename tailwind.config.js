3eb0cb837f5287bc288a4da4cf79aee033076b58
candra256 <dalangpelo982@gmail.com>
2024-09-23 03:27:58 -1200

    mengubah tema website (warna)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     extend: {
       spacing : {
         '29' : '6.1rem',
+      },
+
+      height : {
+        '128' : '40rem',
+        '120' : '35rem',
+        '110' : '30rem'
       }
+
+
     },
   },
   plugins: [
