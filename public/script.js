f608f0725c93693e3704d1e215681da37e8c21b0
candra256 <dalangpelo982@gmail.com>
2024-09-24 18:38:54 -1200

    mengupgrade halaman what peopele say about us

--- public/script.js
+++ public/script.js
@@ -1,51 +1,43 @@
-
-
 //_____________________________________________________________ 🔽
 
-const toggle = document.getElementsByClassName('toggle')[0];
-const menu = document.getElementsByClassName('menu')[0];
+const toggle = document.getElementsByClassName("toggle")[0];
+const menu = document.getElementsByClassName("menu")[0];
 
 // Menambahkan event listener untuk tombol toggle
-toggle.addEventListener('click', function(event) {
-    // Toggle class hidden pada menu
-    menu.classList.toggle('hidden');
-    
-    // Mencegah event bubbling agar klik di toggle tidak dianggap sebagai klik di luar menu
-    event.stopPropagation();
+toggle.addEventListener("click", function (event) {
+  // Toggle class hidden pada menu
+  menu.classList.toggle("hidden");
+
+  // Mencegah event bubbling agar klik di toggle tidak dianggap sebagai klik di luar menu
+  event.stopPropagation();
 });
 
 // Event listener untuk klik di luar elemen menu
-document.addEventListener('click', function(event) {
-    // Jika menu terbuka (tidak ada class hidden) dan pengguna mengklik di luar menu
-    if (!menu.classList.contains('hidden') && !menu.contains(event.target)) {
-        // Tambahkan class hidden untuk menyembunyikan menu
-        menu.classList.add('hidden');
-    }
+document.addEventListener("click", function (event) {
+  // Jika menu terbuka (tidak ada class hidden) dan pengguna mengklik di luar menu
+  if (!menu.classList.contains("hidden") && !menu.contains(event.target)) {
+    // Tambahkan class hidden untuk menyembunyikan menu
+    menu.classList.add("hidden");
+  }
 });
 
 //_____________________________________________________________ 🔼
 
-
 //_____________________________________________________________ 🔽
 
-// const images = [
-//     '/img/pain2.jpg',
-//     '/img/pain1.jpg',
-//     '/img/pain3.jpg'
-// ]
+function loadVideo(element) {
+  // Dapatkan ID video dari atribut data-video-id
+  var videoId = element.getAttribute("data-video-id");
+  var iframe = document.createElement("iframe");
+  iframe.setAttribute("width", "100%");
+  iframe.setAttribute("height", "236rem");
+  iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + videoId + "?autoplay=1");
+  iframe.setAttribute("frameborder", "0");
+  iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+  iframe.setAttribute("allowfullscreen", true);
 
-
-// let currentIndex = 0
-
-// function changeImage(){
-//     const imgElement = document.getElementById('dynamic')
-//     currentIndex = (currentIndex + 1) % images.length
-//     imgElement.src = images[currentIndex]
-
-// }
-
-// setInterval(changeImage , 3000)
+  // Ganti thumbnail dengan iframe
+  element.parentNode.replaceChild(iframe, element);
+}
 
 //_____________________________________________________________ 🔼
-
-  
